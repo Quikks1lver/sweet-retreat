f3aab92eef74cbf4d8b4116250cb791dca18aa27
Adam Fernandes <fernadam8@gmail.com>
2021-05-09 11:23:51 -0400

    y movement

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import background.Background_Methods as bg_methods
 # constants
 WIDTH, HEIGHT = 800, 600
 PLAYER_X_START, PLAYER_Y_START = 50, 460
-PLAYER_X_VELOCITY, PLAYER_Y_VELOCITY = 2, 2
+PLAYER_X_VELOCITY, PLAYER_Y_VELOCITY = 1.1, 0.3
+PLAYER_Y_TOP_THRESHOLD, PLAYER_Y_BOTTOM_THRESHOLD = 440, 500
 
 # initialize the pygame & create screen
 pygame.init()
@@ -23,7 +24,7 @@ stage_pos_x = 0
 start_scrolling_pos_x = WIDTH / 2
 
 # init player character
-player = Player("images/ghost.png", PLAYER_X_START, PLAYER_Y_START, start_scrolling_pos_x, stage_width, WIDTH)
+player = Player("images/ghost.png", PLAYER_X_START, PLAYER_Y_START, start_scrolling_pos_x, stage_width, WIDTH, PLAYER_Y_TOP_THRESHOLD, PLAYER_Y_BOTTOM_THRESHOLD)
 
 running = True
 while running:
@@ -35,13 +36,13 @@ while running:
 
         # key presses
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player.set_x_velocity(-PLAYER_X_VELOCITY)
-            if event.key == pygame.K_RIGHT:
-                player.set_x_velocity(PLAYER_X_VELOCITY)
+            if event.key == pygame.K_LEFT: player.set_x_velocity(-PLAYER_X_VELOCITY)
+            if event.key == pygame.K_RIGHT: player.set_x_velocity(PLAYER_X_VELOCITY)
+            if event.key == pygame.K_UP: player.set_y_velocity(-PLAYER_Y_VELOCITY)
+            if event.key == pygame.K_DOWN: player.set_y_velocity(PLAYER_Y_VELOCITY)
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                player.set_x_velocity(0)
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT: player.set_x_velocity(0)
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN: player.set_y_velocity(0)
 
     player.move_player()
     stage_pos_x += bg_methods.determine_stage_change(player)
