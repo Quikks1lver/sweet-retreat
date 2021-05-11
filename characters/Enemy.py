3e0e0a8ec2107840102357457c16a5e70835012d
Adam Fernandes <fernadam8@gmail.com>
2021-05-10 20:12:43 -0400

    enemy run 2

--- characters/Enemy.py
+++ characters/Enemy.py
@@ -1,43 +1,43 @@
 import math
 import pygame
+from .Player import Player
+import random
 
-class Enemy():
+class Enemy(Player):
     """
     Represents an enemy character in the pygame
     """
 
-    def __init__(self, image_path: str, x_start: int, y_start: int, x_velocity: int, y_velocity: int, stage_width: int, game_width: int, y_top_threshold: int, y_bottom_threshold: int):
-        # TODO
-        # DOCUMENTATION
+    def __init__(self, image_path: str, x_start: int, y_start: int, start_scrolling_pos_x: int, stage_width: int, game_width: int, y_top_threshold: int, y_bottom_threshold: int, health: int, x_velocity: float, y_velocity: float):
+        super().__init__(image_path, x_start, y_start, start_scrolling_pos_x, stage_width,
+                         game_width, y_top_threshold, y_bottom_threshold, health)
 
-        """
-        Initialize an enemy character
-        """
-        self.image = pygame.image.load(image_path)
-        self.image_width = self.image.get_width()
-
-        self.x = x_start
-        self.y = y_start
         self.x_velocity = x_velocity
         self.y_velocity = y_velocity
 
-        self.stage_width = stage_width
-        self.game_width = game_width
+    def move(self, player: Player) -> None:
 
-        self.y_top_threshold = y_top_threshold
-        self.y_bottom_threshold = y_bottom_threshold
+        # inject some randomness
+        self.x += random.randint(-1, 1)
+        self.y += random.randint(-1, 1)
 
-        self.is_left_facing = False
-
-    def draw(self, screen) -> None:
-        if self.is_left_facing: screen.blit(self.image, (self.x, self.y))
-        else: screen.blit(pygame.transform.flip(self.image, True, False), (self.x, self.y))
-
-    def move(self, player) -> None:
-        if self.x > player.real_x_position: self.x -= self.x_velocity
-        elif self.x < player.real_x_position: self.x += self.x_velocity
+        if self.x > player.x: self.x -= self.x_velocity
+        elif self.x < player.x: self.x += self.x_velocity
         else: pass
 
         if self.y > player.y: self.y -= self.y_velocity
         elif self.y < player.y: self.y += self.y_velocity
         else: pass
+
+        # make sure enemy doesn't go above x/y thresholds
+        if self.x > self.stage_width - self.image_width: self.x = self.stage_width - self.image_width
+        if self.x < 0: self.x = 0
+        if self.y < self.y_top_threshold: self.y = self.y_top_threshold
+        if self.y > self.y_bottom_threshold: self.y = self.y_bottom_threshold
+
+        if self.x < self.start_scrolling_pos_x:
+            self.real_x_position = self.x
+        elif self.x > self.stage_width - self.start_scrolling_pos_x:
+            self.real_x_position = self.x - self.stage_width + self.game_width
+        else:
+            self.real_x_position = self.start_scrolling_pos_x
