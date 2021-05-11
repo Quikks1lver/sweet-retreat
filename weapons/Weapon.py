fa85eabbce970ec5a921ff9717a9d3f3fd3ca20b
Adam Fernandes <fernadam8@gmail.com>
2021-05-11 10:41:34 -0400

    obvious methods for weapons

--- weapons/Weapon.py
+++ weapons/Weapon.py
@@ -6,9 +6,14 @@ class Weapon():
     Represents a (typically ranged) weapon that either a character uses
     """
 
-    def __init__(self, image_path: str, character: Player, projectile_x_velocity: float, damage: float):
+    def __init__(self, image_path: str, character: Player, projectile_x_velocity: float, damage: float, ammo: int):
         """
-        Initializes a weapon
+        Initializes a new weapon
+        :param image_path:
+        :param character: character who's using the weapon
+        :param projectile_x_velocity: how fast the bullet/projectile travels
+        :param damage: damage hit
+        :param ammo: how much ammunition the weapon holds
         """
         self.character = character
 
@@ -17,6 +22,7 @@ class Weapon():
 
         self.x_velocity = projectile_x_velocity
         self.damage = damage
+        self.ammo = ammo
 
     def draw(self, screen) -> None:
         """
@@ -26,3 +32,11 @@ class Weapon():
         """
         if self.character.is_left_facing: screen.blit(self.image, (self.character.real_x_position - self.image_width, self.character.y + 15))
         else: screen.blit(pygame.transform.flip(self.image, True, False), (self.character.real_x_position + self.image_width + (self.character.image_width/2), self.character.y + 15))
+
+    def add_ammo(self, amount: int) -> None:
+        """
+        Add ammo to weapon
+        :param amount: amount of ammo
+        :return:
+        """
+        self.ammo += amount
