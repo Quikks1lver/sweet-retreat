0e8c72a8961547463e8e2cbea3749f3e7cf91d4b
Adam Fernandes <t-afernandes@microsoft.com>
2021-05-29 17:33:28 -0400

    text method in mys box

---
+++ text/Text.py
@@ -0,0 +1,26 @@
+import pygame
+from enum import Enum
+from typing import Tuple
+
+class Text():
+    """
+    Helpers for rendering text
+    """
+    
+    class Font(Enum):
+        Dewangga = "dewangga.otf"
+
+    @staticmethod
+    def render(screen, text: str, font, font_size: int, color: Tuple[int, int, int], location: Tuple[int, int]) -> None:
+        """
+        Renders text to the screen
+        :param screen:
+        :param text: text to be displayed
+        :param font: Text.Font enum
+        :param font_size: in pixels
+        :param color: RGB tuple
+        :param location: x, y tuple
+        """
+        font = pygame.font.Font(f"./text/{font.value}", font_size)
+        text = font.render(text, True, color)
+        screen.blit(text, location)
