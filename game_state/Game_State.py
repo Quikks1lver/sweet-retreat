31de724366bfef97da6f83adf8d25f3f017f343b
Adam Fernandes <t-afernandes@microsoft.com>
2021-06-13 15:30:05 -0400

    game state file

--- game_state/Game_State.py
+++ game_state/Game_State.py
@@ -1,27 +1,48 @@
+
+from sys import maxsize
 from typing import List
 
 from characters.Enemy import Enemy
 from characters.EnemyFactory import EnemyFactory
+from timing.Clock_Methods import Clock_Methods
 
 class Game_State():
     """
     Deals with game progression
     """
-    ice_cream_monster_one: bool = False
-    ice_cream_monster_two: bool = False
-    ice_cream_monster_three: bool = False
+    cooldown = 0
+    COOLDOWN_AMOUNT = 15000 # 15 seconds
 
     @staticmethod
-    def progress(enemies: List[Enemy], enemy_factory: EnemyFactory, enemies_defeated: int):
+    def progress(enemies: List[Enemy], enemy_factory: EnemyFactory, num_enemies_defeated: int) -> None:
         """
         Makes the game harder as more enemies are defeated
         """
-        if enemies_defeated > 25 and not Game_State.ice_cream_monster_one:
-            enemies.append(enemy_factory.create_ice_cream_monster())
-            Game_State.ice_cream_monster_one = True
-        if enemies_defeated > 50 and not Game_State.ice_cream_monster_two:
-            enemies.append(enemy_factory.create_ice_cream_monster())
-            Game_State.ice_cream_monster_two = True
-        if enemies_defeated > 75 and not Game_State.ice_cream_monster_three:
-            enemies.append(enemy_factory.create_ice_cream_monster())
-            Game_State.ice_cream_monster_three = True
+        if num_enemies_defeated == 0: return
+        if not Clock_Methods.is_past_this_time(Game_State.cooldown): return
+
+        if num_enemies_defeated % 25 == 0: Game_State.__add_ice_cream_monster(enemies, enemy_factory)
+        if num_enemies_defeated % 50 == 0: Game_State.__add_brownie_tank(enemies, enemy_factory)
+        
+    @staticmethod
+    def __set_cooldown() -> None:
+        """
+        Sets a cooldown period after the creation of each new enemy
+        """
+        Game_State.cooldown = Clock_Methods.get_current_time() + Game_State.COOLDOWN_AMOUNT
+
+    @staticmethod
+    def __add_brownie_tank(enemies: List[Enemy], enemy_factory: EnemyFactory) -> None:
+        """
+        Adds a brownie tank to the game
+        """
+        enemies.append(enemy_factory.create_brownie_tank())
+        Game_State.__set_cooldown()
+
+    @staticmethod
+    def __add_ice_cream_monster(enemies: List[Enemy], enemy_factory: EnemyFactory) -> None:
+        """
+        Adds an ice cream monster to the game
+        """
+        enemies.append(enemy_factory.create_ice_cream_monster())
+        Game_State.__set_cooldown()
