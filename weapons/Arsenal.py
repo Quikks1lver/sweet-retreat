4d29d71a1bf564b2ea2c5fee99fd70904db0c6e8
Adam Fernandes <t-afernandes@microsoft.com>
2021-06-13 09:41:37 -0400

    alphabetized arsenal weapons

--- weapons/Arsenal.py
+++ weapons/Arsenal.py
@@ -7,15 +7,6 @@ class Arsenal():
     """
 
     @staticmethod
-    def revolver(player: Player) -> Weapon:
-        """
-        Returns a revolver weapon (player's starting weapon)
-        :param player:
-        :return:
-        """
-        return Weapon("Revolver", "images/revolver.png",  "sounds/revolver.wav", "images/bullet.png", player, 5, 10, 50)
-
-    @staticmethod
     def ray_gun(player: Player) -> Weapon:
         """
         Returns a ray gun weapon
@@ -25,13 +16,13 @@ class Arsenal():
         return Weapon("Ray Gun", "images/ray_gun.png",  "sounds/ray_gun.wav", "images/ray_gun_bullet.png", player, 2, 100, 25)
 
     @staticmethod
-    def sniper(player: Player) -> Weapon:
+    def revolver(player: Player) -> Weapon:
         """
-        Returns a sniper rifle weapon
+        Returns a revolver weapon (player's starting weapon)
         :param player:
         :return:
         """
-        return Weapon("Sniper", "images/sniper.png", "sounds/sniper.wav", "images/bullet.png", player, 10, 25, 30)
+        return Weapon("Revolver", "images/revolver.png",  "sounds/revolver.wav", "images/bullet.png", player, 5, 10, 50)
 
     @staticmethod
     def rifle(player: Player) -> Weapon:
@@ -41,3 +32,12 @@ class Arsenal():
         :return:
         """
         return Weapon("Rifle", "images/rifle.png", "sounds/rifle.wav", "images/bullet.png", player, 30, 15, 75)
+    
+    @staticmethod
+    def sniper(player: Player) -> Weapon:
+        """
+        Returns a sniper rifle weapon
+        :param player:
+        :return:
+        """
+        return Weapon("Sniper", "images/sniper.png", "sounds/sniper.wav", "images/bullet.png", player, 10, 25, 30)
