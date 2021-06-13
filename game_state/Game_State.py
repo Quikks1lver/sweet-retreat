8a6eea2c4e3ce7ac057d350d1beefe404d9a4be9
Adam Fernandes <t-afernandes@microsoft.com>
2021-06-13 14:49:55 -0400

    added more ic monsters to progression

--- game_state/Game_State.py
+++ game_state/Game_State.py
@@ -9,15 +9,19 @@ class Game_State():
     """
     ice_cream_monster_one: bool = False
     ice_cream_monster_two: bool = False
+    ice_cream_monster_three: bool = False
 
     @staticmethod
     def progress(enemies: List[Enemy], enemy_factory: EnemyFactory, enemies_defeated: int):
         """
         Makes the game harder as more enemies are defeated
         """
-        if enemies_defeated > 20 and not Game_State.ice_cream_monster_one:
+        if enemies_defeated > 25 and not Game_State.ice_cream_monster_one:
             enemies.append(enemy_factory.create_ice_cream_monster())
             Game_State.ice_cream_monster_one = True
-        if enemies_defeated > 40 and not Game_State.ice_cream_monster_two:
+        if enemies_defeated > 50 and not Game_State.ice_cream_monster_two:
             enemies.append(enemy_factory.create_ice_cream_monster())
             Game_State.ice_cream_monster_two = True
+        if enemies_defeated > 75 and not Game_State.ice_cream_monster_three:
+            enemies.append(enemy_factory.create_ice_cream_monster())
+            Game_State.ice_cream_monster_three = True
