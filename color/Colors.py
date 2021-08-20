a558f396ac6c87449ce9b345493f844f80ccc22f
Adam Fernandes <fernadam8@gmail.com>
2021-08-20 11:09:20 -0400

    colors file

---
+++ color/Colors.py
@@ -0,0 +1,10 @@
+class Colors():
+    """
+    Colors which boil down to RGB tuples
+    """
+
+    Neon_Cyan = (8, 247, 254)
+    Neon_Green = (57, 255, 20)
+    Neon_Magenta = (255, 29, 206)
+    Red = (255, 0, 0)
+    White = (255, 255, 255)
