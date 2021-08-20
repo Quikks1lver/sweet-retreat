9ccbe4d500d77cd90c65c012b74461c9ffeb1c8f
Adam Fernandes <fernadam8@gmail.com>
2021-08-20 11:09:31 -0400

    edited text file to include colors tuples

--- text/Text.py
+++ text/Text.py
@@ -1,4 +1,5 @@
 import pygame
+from color.Colors import Colors
 from enum import Enum
 from typing import Tuple, Union
 
@@ -10,22 +11,15 @@ class Text():
     class Font(Enum):
         Dewangga = "dewangga.otf"
 
-    class Color():
-        Neon_Cyan = (8, 247, 254)
-        Neon_Green = (57, 255, 20)
-        Neon_Magenta = (255, 29, 206)
-        Red = (255, 0, 0)
-        White = (255, 255, 255)
-
     @staticmethod
-    def render(screen, text: str, font, font_size: int, color: Union[Color, Tuple[int, int, int]], location: Tuple[int, int]) -> int:
+    def render(screen, text: str, font, font_size: int, color: Union[Colors, Tuple[int, int, int]], location: Tuple[int, int]) -> int:
         """
         Renders text to the screen, returns width of text
         :param screen:
         :param text: text to be displayed
         :param font: Text.Font enum
         :param font_size: in pixels
-        :param color: Text.Color or RGB tuple
+        :param color: Colors.Color value or RGB tuple
         :param location: x, y tuple
         """
         font = pygame.font.Font(f"./text/{font.value}", font_size)
