351f2c6d6c5d237c996fbe548a681379341c25b0
Adam Fernandes <fernadam8@gmail.com>
2021-08-20 14:30:58 -0400

    start of timed text file

---
+++ text/TimedText.py
@@ -0,0 +1,23 @@
+from .Text import Text
+
+class Timed_Text():
+   """
+   Class w/state in order to render text for a given time span
+   """
+
+   def __init__():
+      """
+
+      """
+
+   def render_timed_text():
+      """
+      Renders text to the screen for a set amount of time, returns width of text
+      :param screen:
+      :param text: text to be displayed
+      :param font: Text.Font enum
+      :param font_size: in pixels
+      :param color: Colors.Color value or RGB tuple
+      :param location: x, y tuple
+      :param timespan: time in seconds for text to be displayed
+      """
